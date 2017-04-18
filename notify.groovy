9b2a6d2d73843c4d6449224b7097d3d0eeca7330
zhangzju <zhangway19921221@163.com>
2017-04-18 09:56:39 +0800

    add title

--- notify.groovy
+++ notify.groovy
@@ -1,12 +1,12 @@
 node {
     try {
-        docker.image("ericchu0302/ubuntu14.04-mtk:v2").inside("-t -i -e TERM=linux -u root -v /var/lib/jenkins/workspace/HomeGatewayCheckout:/home/bba/git-src") {
+        docker.image("ericchu0302/ubuntu14.04-mtk:v2").inside("-t -i -e TERM=linux -u root -v /var/lib/jenkins/workspace/HomeGatewayCheckout/linux_mtk_VR500vTTHGW:/home/bba/git-src") {
             sh 'cd /home/bba/git-src/build; make MODEL=VR600_TT_V1 env_build'
             sh 'cd /home/bba/git-src/build; make MODEL=VR600_TT_V1 boot_build'
             sh 'cd /home/bba/git-src/build; make MODEL=VR600_TT_V1 kernel_build'
             sh 'cd /home/bba/git-src/build; make MODEL=VR600_TT_V1 modules_build'
-            sh 'mkdir -p /home/bba/git-src/work/20170316_HGW/linux_mtk_VR500vTTHGW/apps/public/zebra-0.95a; touch /home/bba/git-src/work/20170316_HGW/linux_mtk_VR500vTTHGW/apps/public/zebra-0.95a/missing'
-            sh 'mkdir -p /mnt/hgfs/share/mySvn/linux/zebra; touch /mnt/hgfs/share/mySvn/linux/zebra/missing'
+            //sh 'mkdir -p /home/bba/git-src/work/20170316_HGW/linux_mtk_VR500vTTHGW/apps/public/zebra-0.95a; touch /home/bba/git-src/work/20170316_HGW/linux_mtk_VR500vTTHGW/apps/public/zebra-0.95a/missing'
+            //sh 'mkdir -p /mnt/hgfs/share/mySvn/linux/zebra; touch /mnt/hgfs/share/mySvn/linux/zebra/missing'
             sh 'cd /home/bba/git-src/build; make MODEL=VR600_TT_V1 apps_build'
             sh 'cd /home/bba/git-src/build; make MODEL=VR600_TT_V1 fs_build'
             sh 'cd /home/bba/git-src/build; make MODEL=VR600_TT_V1 mkkernel_build'
